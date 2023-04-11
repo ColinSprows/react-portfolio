a8114cd75700f17d4057fb3961a178f396b220ba
Colin Sprows <colin.sprows@gmail.com>
2023-04-11 15:23:45 -0400

    imported mui and added header and footer, also started on body components

---
+++ src/components/Footer.js
@@ -0,0 +1,28 @@
+import * as React from 'react';
+import Box from '@mui/material/Box';
+import BottomNavigation from '@mui/material/BottomNavigation';
+import BottomNavigationAction from '@mui/material/BottomNavigationAction';
+import GitHubIcon from '@mui/icons-material/GitHub';
+import LinkedInIcon from '@mui/icons-material/LinkedIn';
+
+export default function SimpleBottomNavigation() {
+  return (
+    <Box sx={{ 
+        position: "fixed",
+        bottom: "0px",
+        width: "100%",
+        display: "flex",
+        color: "white",
+        height: "15%",
+        float: "right",
+        alignItems: "center",
+        justifyContent: "center",
+        }}
+    >
+      <BottomNavigation>
+        <BottomNavigationAction icon={<GitHubIcon />} />
+        <BottomNavigationAction icon={<LinkedInIcon />} />
+      </BottomNavigation>
+    </Box>
+  );
+}
---
+++ src/components/Header.js
@@ -0,0 +1,52 @@
+import * as React from 'react';
+import '../styles/Header.css';
+import AppBar from '@mui/material/AppBar';
+import Box from '@mui/material/Box';
+import Toolbar from '@mui/material/Toolbar';
+import Typography from '@mui/material/Typography';
+import Container from '@mui/material/Container';
+import Button from '@mui/material/Button';
+
+const pages = ['About', 'Contact', 'Portfolio', 'Resume'];
+
+export default function Header() {
+
+  return (
+    <AppBar position="static">
+      <Container maxWidth="xl">
+        <Toolbar disableGutters>
+          <Box sx={{ flexGrow: 1, display: { xs: 'none', md: 'flex' } }}>
+            <Typography
+                variant="h6"
+                noWrap
+                component="a"
+                href="/"
+                sx={{
+                mr: 2,
+                display: { xs: 'none', md: 'flex' },
+                fontFamily: 'monospace',
+                fontWeight: 700,
+                letterSpacing: '.3rem',
+                color: 'inherit',
+                textDecoration: 'none',
+                }}
+            >
+                LOGO
+            </Typography>
+          </Box>
+          <Box sx={{ flexGrow: 0, display: { xs: 'none', md: 'flex' } }}>
+            {pages.map((page) => (
+              <Button
+                key={page}
+                href={page}
+                sx={{ my: 2, color: 'white', display: 'block' }}
+              >
+                {page}
+              </Button>
+            ))}
+          </Box>
+        </Toolbar>
+      </Container>
+    </AppBar>
+  );
+}
