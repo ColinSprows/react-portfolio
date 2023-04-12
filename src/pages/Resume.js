4b0a723db319b4af6ad8f2074c12a81e864bab0f
Colin Sprows <colin.sprows@gmail.com>
2023-04-11 20:29:39 -0400

    boilerplate resume page

--- src/pages/Resume.js
+++ src/pages/Resume.js
@@ -1,8 +1,45 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import List from '@mui/material/List';
+import ListItem from '@mui/material/ListItem';
+import ListItemButton from '@mui/material/ListItemButton';
+import ListItemIcon from '@mui/material/ListItemIcon';
+import ListItemText from '@mui/material/ListItemText';
+import ListSubheader from '@mui/material/ListSubheader';
+import Divider from '@mui/material/Divider';
+import InboxIcon from '@mui/icons-material/Inbox';
+import DraftsIcon from '@mui/icons-material/Drafts';
+import { Typography, Card } from '@mui/material';
 
 const Resume = () => {
   return (
-    <div>Resume</div>
+    <div style={{display: "flex", flexDirection: "column", alignItems: "center", textAlign: "center",}}>
+      <Card sx={{ minWidth: 275, marginTop: 2, backgroundColor: "#f0f7ff", padding:"0px 24px" }}>
+        <Typography sx={{fontSize:"1.5em", fontWeight:"800", marginTop: "12px"}}>Resume</Typography>
+        <Typography sx={{marginBottom: "12px"}}>Download my <span>resume</span></Typography>
+        <Box sx={{ width: '60vw', maxWidth: 600 }}>
+        <Divider />
+          <List subheader={<ListSubheader sx={{background: "transparent"}}>Front-End Proficiences</ListSubheader>}>
+              <ListItemText primary="HTML" />
+              <ListItemText primary="CSS" />
+              <ListItemText primary="JavaScript" />
+              <ListItemText primary="JQuery" />
+              <ListItemText primary="responsive design" />
+              <ListItemText primary="React" />
+              <ListItemText primary="Bootstrap" />
+          </List>
+        <Divider />
+          <List subheader={<ListSubheader sx={{background: "transparent"}}>Back-End Proficiences</ListSubheader>}>
+              <ListItemText primary="APIs" />
+              <ListItemText primary="Node" />
+              <ListItemText primary="Express" />
+              <ListItemText primary="MySQL" />
+              <ListItemText primary="Sequelize" />
+          </List>
+      </Box>
+    </Card>
+      
+    </div>
   )
 }
 
